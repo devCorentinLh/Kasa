a97d9d493f5157840908c453daa8b288d14e040d
CorentinLh <120260690+devCorentinLh@users.noreply.github.com>
2023-03-14 18:45:17 +0100

    Update Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,6 @@ import Header from '../../components/header/Header'
 import Banner from '../../components/banner/Banner'
 import Gallery from '../../components/gallery/Gallery'
 import Footer from '../../components/footer/Footer'
-import { useEffect } from 'react';
-
 
 export default function Home() {
 	return (
